07378aac5b9b4a8407d8fd4197bdb29cfdc76b33
Steven Bull <stevenwbulljr@gmail.com>
2021-12-24 12:50:23 -0600

    initial server setup, plan out GET invoice sql

---
+++ starter-code/server/src/invoice/invoice-router.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+
+const invoiceRouter = express.Router();
+const jsonBodyParser = express.json();
+
+invoiceRouter
+    .get((req, res, next) => {
+
+    })
